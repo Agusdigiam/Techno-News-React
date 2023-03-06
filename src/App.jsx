6db837c3f59c7bf2edeb2e3ad5af1c69e284117a
Agustin Di Giambatista <agustindigiambatista@gmail.com>
2023-03-06 17:53:40 -0300

    Only Header

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <main>
+        <h1 className='Title-main'><img className='ico-main' src="./src/assets/logomain.svg" alt="logo"/>Techno News</h1>
+        <nav className='Nav-main-menu'>
+            <ul>
+                <li>Noticias<a href="#"></a></li>
+                <li>Ultima Hora<a href="#"></a></li>
+                <li>Lanzamientos<a href="#"></a></li>
+                <li>Nosotros<a href="#Form"></a></li>
+            </ul>
+        </nav>
+    </main>
+  )
+}
+
+export default App
